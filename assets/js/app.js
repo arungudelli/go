20f71bb5212cf9351e383a33c41ba92b9c16f83f
arunkumar <arunkumar.gudelli@gmail.com>
2022-09-15 21:46:21 +0530

    removed GA

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,8 +4,3 @@ if ('serviceWorker' in navigator) {
     });
     
 }
-window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-TN8WQW5SPM');
